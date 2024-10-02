45dbfa624459604ce57f2ef9f89253175a02bd6e
Matt <mcalabia@baunfire.com>
2024-10-03 04:32:25 +0800

    script

--- custom.js
+++ custom.js
@@ -5,10 +5,20 @@ $(document).ready(function() {
         autoWidth:true,
         nav: false,
         autoplay:true,
+        items:8,
         autoplayTimeout:5000,
         autoplayHoverPause:false,
         slideTransition: 'linear',
         autoplaySpeed: 6000,
         smartSpeed: 6000
     })
+
+    $('.cci-owl-carousel').owlCarousel({
+        margin:22,
+        loop:true,
+        autoWidth:true,
+        items:4
+    })
 });
+
+
