f04c93aa5961d7bacc9e358286e6e3278ee5b261
Matt <mcalabia@baunfire.com>
2024-10-03 03:34:03 +0800

    script

---
+++ custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        margin:108,
+        loop:true,
+        autoWidth:true,
+        items:4,
+        autoplay:false,
+        autoplayTimeout:5000,
+        autoplayHoverPause:false
+    })
+});
