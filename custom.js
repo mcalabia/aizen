9ce98e7d6fbb7f4c49ac3059847faba8cedb5e09
Matt <mcalabia@baunfire.com>
2024-10-09 07:10:28 +0800

    script

--- custom.js
+++ custom.js
@@ -154,7 +154,7 @@ privacyPolicyElements.forEach(({ selector, trigger }) => {
             { 
                 color: "var(--navy)", 
                 opacity: 1,
-                duration: 0.5, 
+                duration: 0.2, 
                 scrollTrigger: {
                     trigger: pptriggerElement, 
                     start: "top 25%",
@@ -162,8 +162,8 @@ privacyPolicyElements.forEach(({ selector, trigger }) => {
                     // scrub: true, 
                     markers: true, 
                     toggleActions: "play reset play reset", 
-                    onLeave: () => gsap.set(ppelement, { opacity: 0.5, color: "rgba(21, 21, 21)"}), 
-                    onLeaveBack: () => gsap.set(ppelement, { opacity: 0.5, color: "rgba(21, 21, 21)"})
+                    onLeave: () => gsap.set(ppelement, { opacity: 0.35, color: "rgba(21, 21, 21)"}), 
+                    onLeaveBack: () => gsap.set(ppelement, { opacity: 0.35, color: "rgba(21, 21, 21)"})
                 }
             }
         );
