a43d81c923159b439cbd359e5ea3aaa9a2e6c434
Matt <mcalabia@baunfire.com>
2024-11-14 06:20:15 +0800

    animation

--- custom.js
+++ custom.js
@@ -334,16 +334,7 @@ function initRevealOnScroll() {
 
     });
 
-    const lenis = new Lenis();
-
-    lenis.on('scroll', (e) => {});
-
-    function raf(time) {
-        lenis.raf(time);
-        requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
+ 
 }
 
 
