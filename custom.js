8a0ae677303872f3de47f02d6df87f6116c1a0b9
Matt <mcalabia@baunfire.com>
2024-11-14 06:51:27 +0800

    animation

--- custom.js
+++ custom.js
@@ -518,6 +518,8 @@ animateElementWithScrollTrigger(".fitc-block", ".fitc-block", .3);
 animateElementWithScrollTrigger("#featured-1", "#featured-1", .3);
 animateElementWithScrollTrigger("#featured-2", "#featured-2", .3);
 animateElementWithScrollTrigger("#featured-3", "#featured-3", .3);
+animateElementWithScrollTrigger("#featured-4", "#featured-4", .3);
+animateElementWithScrollTrigger("#featured-5", "#featured-5", .3);
 
 animateElementsWithScroll(elements);
 animatePrivacyPolicyElements(privacyPolicyElements);
