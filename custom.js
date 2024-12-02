07d9aac9936f1a07440b343e818fc995f5dd6130
Matt <matthew.calabia@founderos.com>
2024-12-03 06:56:53 +0800

    animation

--- custom.js
+++ custom.js
@@ -517,6 +517,7 @@ $('.header-nav-dropdown').on('click', function () {
   $('[dropdown-toggle="default"]').addClass('w--open');
   $('[dropdown-list="default"]').addClass('w--open');
 
+  $('[dropdown-toggle="default"]').click();
   // Handle click event on elements with class `dropdown-wrap`
   $('.accordion-dropdown-block-items').on('click', function () {
     $('[dropdown-toggle="default"]').removeClass('w--open');
